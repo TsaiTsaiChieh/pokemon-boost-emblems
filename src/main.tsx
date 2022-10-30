89539a8d1d7ea2ff6e3259f9862f83c23a642ea7
Tsai-Chieh <d0177381@gmail.com>
2022-10-30 21:17:48 +0800

    :sparkles: import i18 and format

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import './index.css'
+import './index.css';
+import './i18n';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App'
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
