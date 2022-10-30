9cafd3912444895c3f7ef8c9d362126dc9fc77c1
Tsai-Chieh <d0177381@gmail.com>
2022-10-30 21:12:33 +0800

    :sparkles: add custom color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray-50': '#EDEDED',
+        'custom-black': '#242424',
+        'light-1': '#242424',
+        'light-2': '#99A799',
+        'light-3': '#D3E4CD',
+        'light-4': '#FEF5ED',
+        'dark-1': '#495371',
+        'dark-2': '#74959A',
+        'dark-3': '#98B4AA',
+        'dark-4': '#F1E0AC',
+      },
+      boxShadow: {
+        'custom': '0px 10px 10px #EDEDED'
+      },
+    },
   },
   plugins: [],
 }
