9fb41ed2f239ab5a64b52734c455633bcddd6991
Tsai-Chieh <d0177381@gmail.com>
2022-10-31 00:06:01 +0800

    :sparkles: format and pass pokemon props

--- src/components/EmblemList.tsx
+++ src/components/EmblemList.tsx
@@ -1,15 +1,22 @@
-
 import {useTranslation} from 'react-i18next'
+
 import EmblemCard from './EmblemCard'
 
 const EmblemList = () => {
   const {t} = useTranslation()
-  const pokemon: PokemonType[] = t('pokemon', {returnObjects: true})
+  const abilities: AbilityType[] = t('abilities', {returnObjects: true})
+  const pokemonList: PokemonType[] = t('pokemon', {returnObjects: true})
 
   return (
-    <div className="flex bg-light-4 gap-4 justify-center">
-      {pokemon.map((ele) => <EmblemCard key={ele.id} id={ele.id} name={ele.name} skill_1={ele.skill_1[0]} skill_2={ele.skill_2[0]} categories={ele.categories} />)}
-    </div>
+    <ul className='flex flex-wrap bg-light-4 gap-4 justify-center'>
+      {abilities.map((ele, i) => (
+        <EmblemCard
+          key={i}
+          ability={ele}
+          pokemon={pokemonList[parseInt(ele.id) - 1]}
+        />
+      ))}
+    </ul>
   )
 }
 
