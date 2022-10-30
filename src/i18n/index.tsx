3293b7ac90ca24b9650d96a8a93378f64eb8d2a8
Tsai-Chieh <d0177381@gmail.com>
2022-10-30 21:14:02 +0800

    :tada: init i18

---
+++ src/i18n/index.tsx
@@ -0,0 +1,19 @@
+import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import {initReactI18next} from 'react-i18next'
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    fallbackLng: 'zh',
+    lng: 'zh',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
