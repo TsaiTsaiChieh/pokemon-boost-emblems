110525d3f2745f7a6b536af09e9254de1c00a251
Tsai-Chieh <d0177381@gmail.com>
2022-10-31 15:41:56 +0800

    :sparkles: add theme mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {useEffect, useState} from 'react'
+
 import Emblem from './page/Emblem'
 
 const App = () => {
+  const [theme, setTheme] = useState<string>('light')
+  useEffect(() => {
+    theme === 'light' ?
+      document.documentElement.classList.remove('dark') :
+      document.documentElement.classList.add('dark')
+  }, [theme])
+
   return (
-    <div className='bg-light-3 min-h-screen'>
+    <div className='bg-light-3 min-h-screen dark:bg-slate-900'>
+      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        {theme} Mode
+      </button>
       <Emblem />
     </div>
   )
