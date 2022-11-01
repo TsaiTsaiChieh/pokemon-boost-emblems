4636c42213fb9920b23386992617f7a67b843a31
Tsai-Chieh <d0177381@gmail.com>
2022-11-02 00:55:58 +0800

    :sparkles: add pokemon reducer

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -12,7 +12,11 @@ import storage from 'redux-persist/lib/storage'
 
 import rootReducer from './rootReducer'
 
-const persistConfig = {key: 'root', storage, whitelist: ['persist']}
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['persist', 'pokemon'],
+}
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 export const store = configureStore({
   reducer: persistedReducer,
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from '@reduxjs/toolkit'
 
 import persistReducer from './reducers/persistSlice'
+import pokemonReducer from './reducers/pokemonSlice'
 
 const rootReducer = combineReducers({
   persist: persistReducer,
+  pokemon: pokemonReducer,
 })
 
 export default rootReducer
