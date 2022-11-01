f51fe23fe1c15d5fd17fe53cac2f09eac954e644
Tsai-Chieh <d0177381@gmail.com>
2022-11-02 00:25:51 +0800

    :sparkles: import FreeSoloSearch component

--- src/page/Emblem.tsx
+++ src/page/Emblem.tsx
@@ -1,10 +1,12 @@
 import {Container} from '@mui/material'
 
 import EmblemList from '../components/EmblemList'
+import FreeSoloSearch from '../components/FreeSoloSearch'
 
 const Emblem = () => {
   return (
     <Container>
+      <FreeSoloSearch />
       <EmblemList />
     </Container>
   )
