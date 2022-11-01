920c5cef78691ab921a546a6c5ffb0ca563539f7
Tsai-Chieh <d0177381@gmail.com>
2022-11-02 00:25:39 +0800

    :fire: rm setTheme logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import {useEffect, useState} from 'react'
-
+import Header from './components/Header'
 import Emblem from './page/Emblem'
 
 const App = () => {
-  const [theme, setTheme] = useState<string>('light')
-  useEffect(() => {
-    theme === 'light' ?
-      document.documentElement.classList.remove('dark') :
-      document.documentElement.classList.add('dark')
-  }, [theme])
-
   return (
     <div
       className='bg-light-4 dark:bg-dark-1 min-h-screen
     bg-texture w-full relative'
     >
+      <Header />
       {/* <div
         id='gloss'
         className='w-full h-478 fixed bg-gloss'
       ></div> */}
-      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-        {theme} Mode
-      </button>
       <Emblem />
     </div>
   )
