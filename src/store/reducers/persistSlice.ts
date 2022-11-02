6e4e9f13ef65ac08fb30aa5fc80b5b4bc9fcd59b
Tsai-Chieh <d0177381@gmail.com>
2022-11-02 15:27:24 +0800

    :sparkles: add setCharacters reducer

--- src/store/reducers/persistSlice.ts
+++ src/store/reducers/persistSlice.ts
@@ -1,18 +1,23 @@
 import {createSlice} from '@reduxjs/toolkit'
+import type {PayloadAction} from '@reduxjs/toolkit'
 
 const initialState: PersistState = {
   isDarkMode: false,
+  characters: [],
 }
 
 export const persistSlice = createSlice({
   name: 'persist',
   initialState,
   reducers: {
+    setCharacters: (state, {payload}: PayloadAction<string[]>) => {
+      state.characters = payload
+    },
     toggleDarkMode: (state) => {
       state.isDarkMode = !state.isDarkMode
     },
   },
 })
 
-export const {toggleDarkMode} = persistSlice.actions
+export const {setCharacters, toggleDarkMode} = persistSlice.actions
 export default persistSlice.reducer
