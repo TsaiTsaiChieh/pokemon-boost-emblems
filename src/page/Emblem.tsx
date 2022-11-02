14c6b34fd011b7e36e782a7cfb6ea5ce8e0b7fe0
Tsai-Chieh <d0177381@gmail.com>
2022-11-02 17:00:15 +0800

    :sparkles: dispatch characterList

--- src/page/Emblem.tsx
+++ src/page/Emblem.tsx
@@ -5,27 +5,35 @@ import {useTranslation} from 'react-i18next'
 
 import EmblemList from '../components/EmblemList'
 import FreeSoloSearch from '../components/FreeSoloSearch'
-import {useAppDispatch} from '../store/hook/index'
-import {setCard} from '../store/reducers/pokemonSlice'
+import TagFilter from '../components/TagFilter'
+import {useAppDispatch, useAppSelector} from '../store/hook/index'
+import {setCharacters} from '../store/reducers/persistSlice'
+import {setCard, setPokemonList} from '../store/reducers/pokemonSlice'
 
 const Emblem = () => {
   const dispatch = useAppDispatch()
   const {t} = useTranslation()
+  const {characters} = useAppSelector((state) => state.persist)
+  const {tmpCards} = useAppSelector((state) => state.pokemon)
   const abilities: AbilityType[] = t('abilities', {returnObjects: true})
   const pokemonList: PokemonType[] = t('pokemon', {returnObjects: true})
+  const characterList: string[] = t('characters', {returnObjects: true})
   // save pokemon data
   useEffect(() => {
-    if (abilities.length && pokemonList.length) {
+    if (!characters.length) dispatch(setCharacters(characterList))
+    if (!tmpCards.length) {
       abilities.map((ele: AbilityType) => {
         const pokemon: PokemonType = pokemonList[parseInt(ele.id) - 1]
         dispatch(setCard({...ele, ...pokemon}))
+        dispatch(setPokemonList(pokemonList))
       })
     }
-  }, [abilities, pokemonList])
+  }, [tmpCards, characters])
 
   return (
     <Container>
       <FreeSoloSearch />
+      {characters.map((ele, i) => <TagFilter key={i} label={ele} id={i} />)}
       <EmblemList />
     </Container>
   )
