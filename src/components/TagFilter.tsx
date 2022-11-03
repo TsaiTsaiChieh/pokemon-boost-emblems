7b47d704775f7561d2f72c7968e2e914abedbabd
Tsai-Chieh <d0177381@gmail.com>
2022-11-03 16:15:01 +0800

    :recycle: import toggleSubFilterById for more filter group component to reuse

--- src/components/TagFilter.tsx
+++ src/components/TagFilter.tsx
@@ -1,27 +1,22 @@
-import {useEffect, useState} from 'react'
+import {useState} from 'react'
 
 import {Chip} from '@mui/material'
 
-import {useAppDispatch, useAppSelector} from '../store/hook'
-import {setCharactersFilter, setFilter} from '../store/reducers/pokemonSlice'
+import {useAppDispatch} from '../store/hook'
+import {toggleSubFilterById} from '../store/reducers/pokemonSlice'
 
 interface Props {
   id: number
   label: string
+  subFilterName: keyof FilterArrayField
 }
-const TagFilter = ({label, id}: Props) => {
+const TagFilter = ({label, id, subFilterName}: Props) => {
   const [active, setActive] = useState(false)
   const dispatch = useAppDispatch()
-  const {filter} = useAppSelector((state) => state.pokemon)
   const handleClick = () => {
-    dispatch(setCharactersFilter({id, active: !active}))
+    dispatch(toggleSubFilterById({id, active: !active, subFilterName}))
     setActive(!active)
   }
-
-  // TODO should move 2 parent element
-  useEffect(() => {
-    dispatch(setFilter({...filter}))
-  }, [filter.characters])
 
   return (
     <Chip
