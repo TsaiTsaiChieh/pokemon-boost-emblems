84cca30799c273eee3b3657a813c09ec99a42a00
Tsai-Chieh <d0177381@gmail.com>
2022-11-06 00:01:26 +0800

    :sparkles: add total props and reset logic

--- src/components/TagFilter.tsx
+++ src/components/TagFilter.tsx
@@ -1,28 +1,56 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 import {Chip} from '@mui/material'
+import classNames from 'classnames'
 
-import {useAppDispatch} from '../store/hook'
-import {toggleSubFilterById} from '../store/reducers/pokemonSlice'
+import {useAppDispatch, useAppSelector} from '../store/hook'
+import {
+  resetFilter,
+  setReset,
+  toggleSubFilterById,
+} from '../store/reducers/pokemonSlice'
 
 interface Props {
-  id: number
+  id?: number
   label: string
-  subFilterName: keyof FilterArrayField
+  subFilterName?: keyof FilterArrayField
+  total?: number
 }
-const TagFilter = ({label, id, subFilterName}: Props) => {
+const TagFilter = ({label, id, subFilterName, total}: Props) => {
   const [active, setActive] = useState(false)
+  const {reset} = useAppSelector((state) => state.pokemon)
+  const CN = classNames('rounded-md border-1 ', {
+    active: active,
+  })
   const dispatch = useAppDispatch()
+  const span = <span className='scale-65 w-6'>{total}</span>
   const handleClick = () => {
-    dispatch(toggleSubFilterById({id, active: !active, subFilterName}))
-    setActive(!active)
+    if ((id || id === 0) && subFilterName) {
+      dispatch(
+        toggleSubFilterById({id: id!, active: !active, subFilterName}),
+      )
+      setActive(!active)
+    } else {
+      // reset
+      dispatch(setReset(true))
+      dispatch(resetFilter())
+    }
   }
+  useEffect(() => {
+    if (reset) {
+      setActive(false)
+      dispatch(setReset(false))
+    }
+  }, [reset])
 
   return (
     <Chip
-      className='border-none'
+      size='small'
+      className={CN}
       label={label}
       onClick={handleClick}
+      deleteIcon={span}
+      onDelete={handleClick}
       variant={active ? 'filled' : 'outlined'}
     />
   )
