91ba027aec62d81e3ba77ba4453481b034a3244d
Tsai-Chieh <d0177381@gmail.com>
2022-11-07 11:00:34 +0800

    :bug: import persistor

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,26 @@ import React from 'react'
 import {StyledEngineProvider} from '@mui/material/styles'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
+import {persistStore} from 'redux-persist'
+import {PersistGate} from 'redux-persist/integration/react'
 
 import App from './App'
 import {store} from './store'
 
+const persistor = persistStore(store)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <StyledEngineProvider injectFirst>
-        <App />
-      </StyledEngineProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <StyledEngineProvider injectFirst>
+          {/* fix error:
+          A component suspended while responding to synchronous input */}
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <App />
+          </React.Suspense>
+        </StyledEngineProvider>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
+
+  </React.StrictMode >,
 )
