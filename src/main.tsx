1bf20876b6c4a0a7d2b1f9464262f7375ffe7143
Tsai-Chieh <d0177381@gmail.com>
2022-11-09 15:22:15 +0800

    :sparkles: Suspense fallback use Loading component

--- src/main.tsx
+++ src/main.tsx
@@ -10,22 +10,25 @@ import {persistStore} from 'redux-persist'
 import {PersistGate} from 'redux-persist/integration/react'
 
 import App from './App'
+import Loading from './components/Loading'
 import {store} from './store'
 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+)
 const persistor = persistStore(store)
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <StyledEngineProvider injectFirst>
           {/* fix error:
           A component suspended while responding to synchronous input */}
-          <React.Suspense fallback={<div>Loading...</div>}>
+          <React.Suspense fallback={<Loading />}>
             <App />
           </React.Suspense>
         </StyledEngineProvider>
       </PersistGate>
     </Provider>
-
   </React.StrictMode >,
 )
