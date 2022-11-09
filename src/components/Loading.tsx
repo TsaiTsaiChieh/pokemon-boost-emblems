c6060ce0a65d379f9033547595a851abd56fd9d9
Tsai-Chieh <d0177381@gmail.com>
2022-11-09 15:21:52 +0800

    :tada: init Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames'
+
+import {useAppSelector} from '../store/hook'
+
+const Loading = () => {
+  const {isDarkMode} = useAppSelector((state) => state.persist)
+  const CN = classNames(
+    'w-full h-screen flex justify-center',
+    {'bg-light-4': !isDarkMode},
+    {'bg-dark-1': isDarkMode},
+  )
+
+  return (
+    <div className={CN}>
+      <img src='favicon.svg' className='w-11 animate-spin' />
+    </div>
+  )
+}
+
+export default Loading
