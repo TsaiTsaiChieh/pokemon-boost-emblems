6488a9096ab4977e278a343aebd721327639e8ca
Tsai-Chieh <d0177381@gmail.com>
2022-11-09 15:56:24 +0800

    :recycle: mv init logic 2 pokemonSlice state initial

--- src/page/emblem/Emblem.tsx
+++ src/page/emblem/Emblem.tsx
@@ -1,31 +1,12 @@
-import {useEffect} from 'react'
-
 import {Container} from '@mui/material'
-import {useTranslation} from 'react-i18next'
 
 import FreeSoloSearch from '../../components/FreeSoloSearch'
-import abilities from '../../constants/abilities.json'
-import {useAppDispatch, useAppSelector} from '../../store/hook/index'
-import {setCard} from '../../store/reducers/pokemonSlice'
+import {useAppSelector} from '../../store/hook/index'
 import CardList from './CardList'
 import FilterGroups from './filters/FilterGroups'
 
-
 const Emblem = () => {
-  const dispatch = useAppDispatch()
-  const {t} = useTranslation()
-  const {language} = useAppSelector((state) => state.persist)
-  const {tmpCards, cards} = useAppSelector((state) => state.pokemon)
-  const pokemonList: PokemonType[] = t('pokemon', {returnObjects: true})
-  // save pokemon data and search options
-  useEffect(() => {
-    if (!tmpCards.length) {
-      abilities.map((ele: AbilityType) => {
-        const pokemon: PokemonType = pokemonList[parseInt(ele.id) - 1]
-        dispatch(setCard({...ele, ...pokemon}))
-      })
-    }
-  }, [tmpCards, language])
+  const {cards} = useAppSelector((state) => state.pokemon)
 
   return (
     <Container>
