27820e1da8f7c4c943f84d2433ad77def13ec893
Tsai-Chieh <d0177381@gmail.com>
2022-11-10 15:40:36 +0800

    :boom: try to fix some chunks are larger than 500 KiB problem

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ import {defineConfig} from 'vite'
 export default defineConfig({
   base: '/pokemon-boost-emblems/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+      },
+    },
+  },
 })
